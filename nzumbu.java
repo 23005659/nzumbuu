7d6a81c6de871bd4bf57059268cb44178aeac699
23005659 <163414615+23005659@users.noreply.github.com>
2024-03-16 13:07:06 +0200

    Add files via upload

---
+++ nzumbu.java
@@ -0,0 +1,19 @@
+import java.util.Scanner; 
+
+class nzumbu {
+  public static void main(String[] args) {
+    int c, d, sum;
+    Scanner nzumbu = new Scanner(System.in); 
+    System.out.println("Type a number:");
+    c = nzumbu.nextInt(); 
+
+    System.out.println("Type another number:");
+    d = nzumbu.nextInt(); 
+
+    sum = c - d;  
+    System.out.println("Sum is: " + sum); 
+  }
+
+    }
+
+   
